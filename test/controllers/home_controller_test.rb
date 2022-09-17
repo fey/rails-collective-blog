22faf50b86f11abc8c9ea3af8c2fe938852efbbb
feycot <feycot@gmail.com>
2022-09-17 17:32:23 +0500

    tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,6 +4,7 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
+    sign_in users(:feycot)
     get home_index_url
     assert_response :success
   end
